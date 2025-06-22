f69b072dcd118d605d75b64787e97ba93be3612c
Anshu Vij <vijanshu27@gmail.com>
2025-06-22 18:23:07 +0530

    day 01 - inital commit

---
+++ WeSplitApp/WeSplitApp/WeSplitAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitAppApp.swift
+//  WeSplitApp
+//
+//  Created by Anshu Vij on 22/06/25.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
